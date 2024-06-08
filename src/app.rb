58f2a463ce3ff3a6924d27310f6622c135c47185
kochi-echo <tnodtegrof@gmail.com>
2024-06-08 18:49:38 +0900

    必要なメソッドを一通り用意

--- src/app.rb
+++ src/app.rb
@@ -2,3 +2,35 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+
+# get '/' do
+#   redirect '/memos'
+# end
+
+# get '/memos' do
+#   erb :top
+# end
+
+# get '/memos/:id' do
+#   erb :show
+# end
+
+# get '/memos/new' do
+#   erb :new
+# end
+
+# post '/memos' do
+#   redirect '/memos'
+# end
+
+# get '/memos/:id/edit' do
+#   erb :edit
+# end
+
+# patch '/memos/:id' do
+#   redirect '/memos/:id'
+# end
+
+# delete '/memos/:id' do
+#   redirect '/memos'
+# end
