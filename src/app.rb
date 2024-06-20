642d136103d8cc5800ee93a7e5c0da02dd3b6faf
kochi-echo <tnodtegrof@gmail.com>
2024-06-21 08:17:55 +0900

    表示機能DB化

--- src/app.rb
+++ src/app.rb
@@ -3,8 +3,41 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'pg'
 
 PATH = File.join(__dir__, 'public/data.json')
+
+class Memo
+  def initialize(params)
+    @params = params
+  end
+
+  def self.all
+    conn.exec('SELECT * FROM memos')
+  end
+
+  def save
+    result = conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [@params[:title], @params[:content]])
+    result.cmd_tuples == 1
+  end
+
+  def self.show(params)
+    result = conn.exec_params('SELECT * FROM memos WHERE id = $1;', [params[:id]])
+    result.ntuples > 0 ? result[0].to_h : {}
+  end
+end
+
+def conn
+  @conn ||= PG.connect(dbname: 'postgres')
+end
+
+configure do
+  result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+  conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)') if result.values.empty?
+  # conn.exec_params('INSERT INTO memos_test(title, content) VALUES ($1, $2);', ['メモ1', 'メモ1の内容'])
+  # conn.exec_params('INSERT INTO memos_test(title, content) VALUES ($1, $2);', ['メモ2', "メモ2の内容\nメモ2の内容"])
+end
+
 
 def read_memos
   File.open(PATH) { |f| JSON.parse(f.read) }
@@ -25,7 +58,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = read_memos
+  @memos = Memo.all
   erb :top
 end
 
@@ -42,17 +75,17 @@ delete '/memos/:id' do
 end
 
 get '/memos/:id' do
-  @memo = read_memos[params[:id]]
+  @memo = Memo.show(params)
   erb :show
 end
 
 post '/memos' do
-  memos = read_memos
-  id = (memos.keys.map(&:to_i).max + 1).to_s
-  memos[id] = { 'title' => params[:title], 'content' => params[:content] }
-  write_memos(memos)
-
-  redirect '/memos'
+  @memo = Memo.new(params)
+  if @memo.save
+    redirect '/memos'
+  else
+    redirect '/memos/new'
+  end
 end
 
 get '/memos/:id/edit' do
