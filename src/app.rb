3363d5b804653307a1af1cb123a4f60d62ec5861
kochi-echo <tnodtegrof@gmail.com>
2024-06-21 11:15:54 +0900

    rubocop

--- src/app.rb
+++ src/app.rb
@@ -43,8 +43,8 @@ configure do
   result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
   if result.values.empty?
     conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)')
-    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', ['メモ1', 'メモ1の内容'])
-    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', ['メモ2', "メモ2の内容\nメモ2の内容"])
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %w[メモ1 メモ1の内容])
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %W[メモ2 メモ2の内容\nメモ2の内容])
   end
 end
 
