fefcb1f236c31bb958af4b67e6a28f72b2f1f738
kochi-echo <tnodtegrof@gmail.com>
2024-06-25 15:31:16 +0900

    インスタンス作るたびに接続に行ってたので、クラス変数に変更

--- src/app.rb
+++ src/app.rb
@@ -5,23 +5,23 @@ require 'sinatra/reloader'
 require 'pg'
 
 class PGDataBase
+  @@conn = PG.connect(dbname: 'postgres')
+
   def self.conn
-    @conn ||= PG.connect(dbname: 'postgres')
+    @@conn
   end
 
   def conn
     self.class.conn
   end
 
-  private
-
   def self.set_table
     result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
-    if result.values.empty?
-      conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)')
-      conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %w[メモ1 メモ1の内容])
-      conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %W[メモ2 メモ2の内容\nメモ2の内容])
-    end
+    return unless result.values.empty?
+
+    conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)')
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %w[メモ1 メモ1の内容])
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %W[メモ2 メモ2の内容\nメモ2の内容])
   end
 end
 
