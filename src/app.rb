53bebadad366957498b74f997422f2a775660d0a
kochi-echo <tnodtegrof@gmail.com>
2024-06-27 21:52:31 +0900

    変更：configureメソッド内のreturnを削除
    理由：returnするとそこで処理が止まる

--- src/app.rb
+++ src/app.rb
@@ -41,11 +41,11 @@ end
 
 configure do
   result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
-  return unless result.values.empty?
-
-  conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)')
-  conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %w[メモ1 メモ1の内容])
-  conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %W[メモ2 メモ2の内容\nメモ2の内容])
+  if result.values.empty?
+    conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)')
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %w[メモ1 メモ1の内容])
+    conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', %W[メモ2 メモ2の内容\nメモ2の内容])
+  end
 end
 
 helpers do
