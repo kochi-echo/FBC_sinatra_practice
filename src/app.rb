65b7c52b16d37f4d2cbcfe8fec101f60df6c208a
kochi-echo <tnodtegrof@gmail.com>
2024-07-05 11:40:16 +0900

    変更：DBの接続情報をインスタンス変数で持つようにした
    理由：クラス変数の場合rubocopに怒られるため

--- src/app.rb
+++ src/app.rb
@@ -6,7 +6,6 @@ require 'pg'
 
 class Memo
   attr_reader :id, :title, :content
-  @@conn ||= PG.connect(dbname: 'postgres')
 
   def initialize(params)
     @id = params['id']
@@ -15,26 +14,36 @@ class Memo
   end
 
   def self.all
-    @@conn.exec('SELECT * FROM memos')
+    conn.exec('SELECT * FROM memos')
   end
 
   def self.find(id)
-    result = @@conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
+    result = conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
     Memo.new(result[0])
   end
 
   def save
-    result = @@conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [@title, @content])
+    result = conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [@title, @content])
     result.cmd_tuples == 1
   end
 
   def update(params)
-    result = @@conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3;', [params['title'], params['content'], params['id']])
+    result = conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3;', [params['title'], params['content'], params['id']])
     result.cmd_tuples == 1
   end
 
   def destroy
-    @@conn.exec_params('DELETE FROM memos WHERE id = $1;', [@id])
+    conn.exec_params('DELETE FROM memos WHERE id = $1;', [@id])
+  end
+
+  private
+
+  def self.conn
+    @conn ||= PG.connect(dbname: 'postgres')
+  end
+
+  def conn
+    self.class.conn # インスタンスメソッド化
   end
 end
 
